4714042ba6227b77abb09f7c0b03c698ef2bb6be
NellySP <nelly.petren@gmail.com>
2022-03-29 20:54:32 +0200

    Add hamburger menu
    not finished, but at least working

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,6 +5,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/app.css">
     <link rel="stylesheet" href="../css/footer.css">
-    <title>Document</title>
+    <title>Kollektiva</title>
+    <h1>Kollektiva</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Meny</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Hem</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Hyr ut din bostad</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sök bostad</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Hur fungerar Kollektiva?</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Hjälpcenter</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Kontakt</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
