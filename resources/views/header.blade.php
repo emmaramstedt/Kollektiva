2611a1ed7ab48025324f3b7b8dea0d74df259040
NellySP <nelly.petren@gmail.com>
2022-03-30 17:14:17 +0200

    menu updates
    can't get rid of the spaces between the dov divs

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -37,7 +37,7 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="/index">Hem</a>
+                    <a class="nav-link" href="/">Hem</a>
                 </li>
                 <div class="headerLine">
                     <hr>
