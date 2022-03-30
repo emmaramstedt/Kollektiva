375480415aff15fbce4247bb108affdb2d22fedd
NellySP <nelly.petren@gmail.com>
2022-03-30 16:01:20 +0200

    header styling
    minor

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -7,10 +7,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/app.css">
+    <link rel="stylesheet" href="../css/header.css">
     <link rel="stylesheet" href="../css/footer.css">
     <title>Kollektiva</title>
-    <h1>Kollektiva</h1>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="languageAndSearchWrapper">
+        <div class="languageBox">
+            <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 13L7 7L1 1" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            </svg>
+            <p>Språk</p>
+        </div>
+        <div class="searchBox">
+            <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 13L7 7L1 1" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            </svg>
+            <p>Sök</p>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-#F5FAF4">
         <a class="navbar-brand" href="#">Meny</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -36,5 +50,7 @@
                     <a class="nav-link" href="#">Kontakt</a>
                 </li>
             </ul>
+            <button class="menuButton">Logga in</button>
+            <button class="menuButton">Registrera dig</button>
         </div>
     </nav>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,6 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .css('resources/css/footer.css', 'public/css')
-    .css('resources/css/app.css', 'public/css');
+mix.js("resources/js/app.js", "public/js")
+    .css("resources/css/footer.css", "public/css")
+    .css("resources/css/app.css", "public/css")
+    .css("resources/css/header.css", "public/css");
