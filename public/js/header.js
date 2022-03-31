6128aede6220de50894baf93b14bb036254e595d
NellySP <nelly.petren@gmail.com>
2022-03-31 13:32:25 +0200

    add js-function to hide searchbar

--- public/js/header.js
+++ public/js/header.js
@@ -4,5 +4,18 @@ var __webpack_exports__ = {};
   !*** ./resources/js/header.js ***!
   \********************************/
 console.log("hai");
+var searchButton = document.querySelector("searchFormButton");
+
+searchButton.addEventListener = function () {
+  var x = document.getElementById("searchcontainer");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  console.log("hai");
+};
 /******/ })()
 ;
--- resources/js/header.js
+++ resources/js/header.js
@@ -1,1 +1,13 @@
 console.log("hai");
+
+const searchButton = document.querySelector("searchFormButton");
+
+searchButton.addEventListener = () => {
+    let x = document.getElementById("searchcontainer");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    console.log("hai");
+};
