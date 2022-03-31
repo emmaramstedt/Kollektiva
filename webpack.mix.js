e174a696708d232f729895704949e5fd46cfced3
NellySP <nelly.petren@gmail.com>
2022-03-31 13:19:24 +0200

    add basic input form
    and try to connect js-file

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ const mix = require("laravel-mix");
  */
 
 mix.js("resources/js/app.js", "public/js")
+    .js("resources/js/header.js", "public/js")
     .css("resources/css/footer.css", "public/css")
     .css("resources/css/app.css", "public/css")
     .css("resources/css/header.css", "public/css");
