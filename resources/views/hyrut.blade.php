b1939a772ebb6fdbc71e2c0d3fc8f9d2c1934f5d
NellySP <nelly.petren@gmail.com>
2022-04-14 17:28:57 +0200

    step 13 form style

--- resources/views/hyrut.blade.php
+++ resources/views/hyrut.blade.php
@@ -405,21 +405,21 @@
                 <div class="summeryContentWrapper">
                     <div class="summaryContent">
                         <h3>Hushåll</h3>
-                        <p>---</p>
+                        <p>Medsökande</p>
                     </div>
                     <div class="summaryContent">
                         <h3>Ålder</h3>
-                        <p>---</p>
+                        <p>46-59 år</p>
                     </div>
                 </div>
                 <div class="summeryContentWrapper">
                     <div class="summaryContent">
                         <h3>Sysselsättning</h3>
-                        <p>---</p>
+                        <p>Alla</p>
                     </div>
                     <div class="summaryContent">
                         <h3>Civilstånd</h3>
-                        <p>---</p>
+                        <p>Alla</p>
                     </div>
                 </div>
             </div>
@@ -431,32 +431,47 @@
             <h3 class="margins">Förhandsgranskning</h3>
             <div class="imageWrapperForm">
                 <img class="formImage" src="{{asset('images/formExample/example.png')}}" alt="Bildspel">
-                <div class="slideshowButtons">
-                    <button class="slideButton">
-                        "<" </button>
-                            <p>Bild 1/5</p>
-                            <button class="slideButton">></button>
+            </div>
+            <div class="slideshowButtons">
+                <button class="slideButton"><svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7 1L1 7L7 13" stroke="#212121" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg></button>
+                <p>Bild 1/5</p>
+                <button class="slideButton"><svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 13L7 7L1 1" stroke="#212121" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg></button>
+            </div>
+            <h3 class="margins">Södra Kroksdal/ Skärhamn</h3>
+
+            <div class="wrapper margins">
+                <div class="summeryContentWrapper">
+                    <div class="summaryContent">
+                        <h3>Inflyttning</h3>
+                        <p>Snarast möjligt</p>
+                    </div>
+                    <div class="summaryContent">
+                        <h3>Utflyttning</h3>
+                        <p>Tillsvidare</p>
+                    </div>
                 </div>
-                <div class="summary margins">
-                    <h3>Södra Kroksdal/ Skärhamn</h3>
-                    <h3>Inflyttning</h3>
-                    <p>Snarast möjligt</p>
-                    <h3>Utflyttning</h3>
-                    <p>Tillsvidare</p>
-                    <h3>Storlek</h3>
-                    <p>2 rum, 80 m2</p>
-                    <h3>Möblerat</h3>
-                    <p>Ja</p>
-                    <h3>Delat boende</h3>
-                    <p>Ja</p>
+                <div class="summeryContentWrapper">
+                    <div class="summaryContent">
+                        <h3>Storlek</h3>
+                        <p>2 rum, 80 m2</p>
+                    </div>
+                    <div class="summaryContent">
+                        <h3>Möblerat</h3>
+                        <p>Ja</p>
+                    </div>
                 </div>
-                <div class="rulesAndDescription margins">
-                    <h3>Beskrivning</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
-                    <h3>Regler</h3>
-                    <p>Husdjur tillåtet</p>
-                    <p>Rökning ej tillåtet</p>
-                </div>
+            </div>
+
+            <div class="rulesAndDescription margins">
+                <h3>Beskrivning</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                <h3>Regler</h3>
+                <p>Husdjur tillåtet</p>
+                <p>Rökning ej tillåtet</p>
             </div>
         </div>
 
@@ -476,7 +491,9 @@
         </div>
 
         <div class="buttonWrapper">
-            <button type="button" class="backButton" id="back" onclick="nextStep(-1)">
+            <button type="button" class="backButton" id="back" onclick="nextStep(-1)"><svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7 1L1 7L7 13" stroke="#212121" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
                 Tillbaka
             </button>
             <button type="button" class="nextButton" id="next" onclick="nextStep(1)"></button>
