b070b0520c99383ea46bfbef24646a6f3231add2
NellySP <nelly.petren@gmail.com>
2022-04-18 15:59:13 +0200

    add flippy button arrow to function

--- public/js/userPage.js
+++ public/js/userPage.js
@@ -4,8 +4,17 @@ var __webpack_exports__ = {};
   !*** ./resources/js/userPage.js ***!
   \**********************************/
 var drop = document.querySelector(".dropDownButton");
+var svg = document.querySelector(".buttonSvg");
 drop.addEventListener("click", function () {
-  console.log("clicked");
+  var x = document.getElementById("hidden");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    svg.style.transform = "scaleY(-1)";
+  } else {
+    x.style.display = "none";
+    svg.style.transform = "scaleY(1)";
+  }
 });
 /******/ })()
 ;
--- resources/js/userPage.js
+++ resources/js/userPage.js
@@ -1,4 +1,12 @@
 const drop = document.querySelector(".dropDownButton");
+const svg = document.querySelector(".buttonSvg");
 drop.addEventListener("click", () => {
-    console.log("clicked");
+    var x = document.getElementById("hidden");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        svg.style.transform = "scaleY(-1)";
+    } else {
+        x.style.display = "none";
+        svg.style.transform = "scaleY(1)";
+    }
 });
