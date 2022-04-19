2757d431375dec1c0fab13cbac67e2d36cac9592
Deliciaes <ceramstedt@gmail.com>
2022-04-19 15:02:12 +0200

    nav and ad tweaks

--- resources/views/ad.blade.php
+++ resources/views/ad.blade.php
@@ -1,45 +1,48 @@
-<div class="imageWrapperForm">
-    <img class="formImage" src="{{asset('images/formExample/example.png')}}" alt="Bildspel">
-</div>
-<div class="slideshowButtons">
-    <button class="slideButton"><svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M7 1L1 7L7 13" stroke="#212121" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-        </svg></button>
-    <p>Bild 1/5</p>
-    <button class="slideButton"><svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 13L7 7L1 1" stroke="#212121" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-        </svg></button>
-</div>
-<h3 class="margins">Södra Kroksdal/ Skärhamn</h3>
-
-<div class="wrapper margins">
-    <div class="summeryContentWrapper">
-        <div class="summaryContent">
-            <h3>Inflyttning</h3>
-            <p>Snarast möjligt</p>
+<div class="adContainer">
+    <div class="ad">
+        <div class="imageWrapperForm">
+            <img class="formImage" src="{{asset('images/formExample/example.png')}}" alt="Bildspel">
         </div>
-        <div class="summaryContent">
-            <h3>Utflyttning</h3>
-            <p>Tillsvidare</p>
+        <div class="slideshowButtons">
+            <button class="slideButton"><svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7 1L1 7L7 13" stroke="#212121" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg></button>
+            <p>Bild 1/5</p>
+            <button class="slideButton"><svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 13L7 7L1 1" stroke="#212121" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg></button>
+        </div>
+        <h3 class="margins">Södra Kroksdal/ Skärhamn</h3>
+
+        <div class="wrapper margins">
+            <div class="summeryContentWrapper">
+                <div class="summaryContent">
+                    <h3>Inflyttning</h3>
+                    <p>Snarast möjligt</p>
+                </div>
+                <div class="summaryContent">
+                    <h3>Utflyttning</h3>
+                    <p>Tillsvidare</p>
+                </div>
+            </div>
+            <div class="summeryContentWrapper">
+                <div class="summaryContent">
+                    <h3>Storlek</h3>
+                    <p>2 rum, 80 m2</p>
+                </div>
+                <div class="summaryContent">
+                    <h3>Möblerat</h3>
+                    <p>Ja</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="rulesAndDescription margins">
+            <h3>Beskrivning</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+            <h3>Regler</h3>
+            <p>Husdjur tillåtet</p>
+            <p>Rökning ej tillåtet</p>
         </div>
     </div>
-    <div class="summeryContentWrapper">
-        <div class="summaryContent">
-            <h3>Storlek</h3>
-            <p>2 rum, 80 m2</p>
-        </div>
-        <div class="summaryContent">
-            <h3>Möblerat</h3>
-            <p>Ja</p>
-        </div>
-    </div>
-</div>
-
-<div class="rulesAndDescription margins">
-    <h3>Beskrivning</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
-    <h3>Regler</h3>
-    <p>Husdjur tillåtet</p>
-    <p>Rökning ej tillåtet</p>
-</div>
 </div>
